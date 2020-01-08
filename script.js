d7fc30c93f6823f9dc7469c450806450794ca505
sacc <50913285+filipkmiecik@users.noreply.github.com>
2020-01-08 16:29:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const hole = document.getElementsByClassName("interactiveHole")
+const ball = document.getElementById("interactiveBall")
+const ballBoundings = ball.getBoundingClientRect()
+const holeBoundings = hole.getBoundingClientRect()
